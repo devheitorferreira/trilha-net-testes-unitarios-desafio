87b57801007066c49a1757cbca2f103ed49e532c
Heitor Ferreira <heitorasf24@outlook.com>
2024-06-01 17:26:48 -0300

    Ajustes Heitor testes desafios unitarios

--- TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
@@ -12,8 +12,8 @@ public class ValidacoesStringTests
         //TODO: Corrigir a variável "texto" e "resultadoEsperado" da seção Arrange
 
         // Arrange
-        var texto = "a";
-        var resultadoEsperado = 0;
+        var texto = "abcdes";
+        var resultadoEsperado = 6;
 
         // Act
         var resultado = _validacoes.RetornarQuantidadeCaracteres(texto);
@@ -26,41 +26,42 @@ public class ValidacoesStringTests
     public void DeveContemAPalavraQualquerNoTexto()
     {
         // Arrange
-        var texto = "Esse é um texto qualquer";
-        var textoProcurado = "qualquer";
+        var texto = "Verdao, muitas glorias";
+        var textoProcurado = "glorias";
 
         //TODO: Corrigir a chamada do método "ContemCaractere" da seção Act
         // Act
-         _validacoes.ContemCaractere(texto, textoProcurado);
+        var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
 
         // Assert
         //TODO: Corrigir o Assert.True com base no retorno da chamada ao método
-        Assert.True(true);
+        Assert.True(resultado);
     }
 
     [Fact]
     public void NaoDeveConterAPalavraTesteNoTexto()
     {
         // Arrange
-        var texto = "Esse é um texto qualquer";
-        var textoProcurado = "teste";
+        var texto = "Verdao, muitas glorias";
+        var textoProcurado = "glorias";
 
         // Act
         var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
 
         // Assert
         //TODO: Corrigir o Assert.False com base no retorno da chamada ao método
-        Assert.False(true);
+        Assert.True(resultado);
     }
 
     //TODO: Corrigir a anotação [Fact]
+    [Fact]
     public void TextoDeveTerminarComAPalavraProcurado()
     {
         //TODO: Corrigir a variável "textoProcurado" seção Arrange
 
         // Arrange
-        var texto = "Começo, meio e fim do texto procurado";
-        var textoProcurado = "teste";
+        var texto = "Vermelho, Verde, Roxo";
+        var textoProcurado = "Roxo";
 
         // Act
         var resultado = _validacoes.TextoTerminaCom(texto, textoProcurado);
